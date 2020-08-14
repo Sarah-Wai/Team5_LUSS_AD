7563f642843ca69fe5e8da33039e8bafbd222ea4
Sherren Soesanto <sherrensoesanto@gmail.com>
2020-08-14 12:47:37 +0800

    added Request and RequestDetails Controller
    added View page for clerk adjustment module

--- Team5_LUSS/Controllers/AdjustVoucherController.cs
+++ Team5_LUSS/Controllers/AdjustVoucherController.cs
@@ -10,7 +10,7 @@ namespace Team5_LUSS.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("AdjustVoucherListingClerk");
         }
     }
 }

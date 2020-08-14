b19e5474778e5ed2ebed48260eaacafced387350
huajiao925 <lyrali142857@outlook.com>
2020-08-14 14:47:51 +0800

    merge

--- Team5_LUSS/Controllers/CollectionController.cs
+++ Team5_LUSS/Controllers/CollectionController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LUSS_API.DB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -9,42 +12,55 @@ using Team5_LUSS.Models;
 
 namespace Team5_LUSS.Controllers
 {
+
     public class CollectionController : Controller
     {
 
-        public async Task<IActionResult> CollectionPoints(int DeptID)
+        string api_url = "https://localhost:44312/CollectionPoint";
+        public async Task<IActionResult> collectionPoints()
         {
             //Get All the Collection Points
-            List<CollectionPoint> collectionPointList = new List<CollectionPoint>();
-            CollectionPoint collectionPoint;
+            List<CollectionPoint> collectionPointInfo = new List<CollectionPoint>();
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync("https://localhost:44312/CollectionPoint"))
+                using (var response = await httpClient.GetAsync(api_url))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
-                    collectionPointList = JsonConvert.DeserializeObject<List<CollectionPoint>>(apiResponse);
-
+                    collectionPointInfo = JsonConvert.DeserializeObject<List<CollectionPoint>>(apiResponse);
                 }
             }
-            ViewData["collectionPoints"] = collectionPointList;
+            //last object is the Department Collection Point
+            ViewData["collectionPoints"] = collectionPointInfo;
+
+            //dummy code for testing, replace with the session data
+            Department d = new Department
+            {
+                DepartmentID = 99,
+                DepartmentName = "DummyDept",
+                PhoneNo = "123",
+                Fax = "456",
+                DepartmentCode = "DD",
+                CollectionPointID = 1
+            };
+
+            return View(d);
+        }
 
 
+
+        [HttpPost]
+        public async Task<IActionResult> collectionPoints(int deptID, int cpID)
+        {
+            Department updated_Dept = new Department();
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync("https://localhost:44312/CollectionPoint/" + DeptID))
+                using (var response = await httpClient.GetAsync(api_url + "/" + deptID))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
-                    collectionPoint = JsonConvert.DeserializeObject<CollectionPoint>(apiResponse);
-
+                    updated_Dept = JsonConvert.DeserializeObject<Department>(apiResponse);
                 }
-
-
-                //Get the Collection Point of that Department
-                ViewData["deptCollectionPoints"] = collectionPoint;
-
-
-                return View();
             }
+            return View(updated_Dept);
         }
     }
 }

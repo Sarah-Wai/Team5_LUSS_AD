8bda88e89e64bbdae7b2e1ee2d6b948a7753c10f
NangSandarAung <nangsandaraungpj@gmail.com>
2020-08-14 11:52:13 +0800

    Nang item List part commit

---
+++ WebApplication1/Controllers/ItemListController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LUSS_API.DB;
+using LUSS_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LUSS_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemListController : ControllerBase
+    {
+        public MyDbContext context123;
+        private readonly ILogger<ItemListController> _logger;
+        public ItemListController(ILogger<ItemListController> logger, MyDbContext context123)
+        {
+            _logger = logger;
+            this.context123 = context123;
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetAllItems()
+        {
+            List<Item> items = context123.Item.ToList();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public Item GetItemByID(int id)
+        {
+            Item item = context123.Item.First(i => i.ItemID == id);
+
+            return item;
+        }
+        
+    }
+}

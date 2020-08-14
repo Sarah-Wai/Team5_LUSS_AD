7d96e1fd65e22fef74d00a1ff4ee2155e9b7e8f7
Sherren Soesanto <sherrensoesanto@gmail.com>
2020-08-14 13:29:35 +0800

    added the following controllers: - AdjustmentVoucher - PO - POItems - Retrieval

---
+++ WebApplication1/Controllers/RetrievalController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LUSS_API.DB;
+using LUSS_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LUSS_API.Controllers
+{   
+    [ApiController]
+    [Route("[controller]")]
+    public class RetrievalController : Controller
+    {
+        public MyDbContext context123;
+        private readonly ILogger<CollectionPointController> _logger;
+        public RetrievalController(ILogger<CollectionPointController> logger, MyDbContext context123)
+        {
+            _logger = logger;
+            this.context123 = context123;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

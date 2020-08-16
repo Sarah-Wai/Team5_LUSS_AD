8be53a454f33c5633049be3fa4953d9e6e782be1
Ian Tan <e0533419@u.nus.edu>
2020-08-16 14:25:12 +0800

    change login and home controllers for login Feature

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using LUSS_API.DB;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using LUSS_API.DB;
+using LUSS_API.Models;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace LUSS_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController
+    {
+        public MyDbContext context123;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger, MyDbContext context123)
+        {
+            _logger = logger;
+            this.context123 = context123;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<User> GetUserList()
+        {
+            List<User> Users = context123.User.ToList();
+            return Users;
+
+        }
+
+    }
+}

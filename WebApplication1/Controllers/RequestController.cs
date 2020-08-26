d24d096287181da420fff48970b8336973daf77b
huajiao925 <lyrali142857@outlook.com>
2020-08-26 10:13:51 +0800

    mobile methods

--- WebApplication1/Controllers/RequestController.cs
+++ WebApplication1/Controllers/RequestController.cs
@@ -555,8 +555,25 @@ namespace LUSS_API.Controllers
             context123.SaveChanges();
             return "ok";
         }
+
+
+        [HttpGet("Mobile_GetAccptQty/{acceptedQty}/{retrievalID}")]
+        public void GetAllocateStationary(string acceptedQty, int retrievalID)
+        {
+            //parse string to array
+            string[] separators = { ",", "[", "]" };
+            string[] str = acceptedQty.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            List<int> qty = new List<int>();
+            foreach (var s in str)
+            {
+                qty.Add(int.Parse(s));
+            }
+            
+            allocateStationary(qty, retrievalID);
+
+        }
     }
 
-    
 
-    }
+
+}
